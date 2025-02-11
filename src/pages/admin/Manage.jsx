3a8c2b608fc806950cd30b950b680b3f85882415
phongphat <phongphat.jap@gmail.com>
2025-02-11 15:15:02 +0700

    update table

--- src/pages/admin/Manage.jsx
+++ src/pages/admin/Manage.jsx
@@ -1,7 +1,25 @@
 // rfce
 function Manage() {
   return (
-    <div>Manage</div>
-  )
+    <div>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
-export default Manage
+export default Manage;
