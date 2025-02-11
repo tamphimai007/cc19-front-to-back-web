d9156ae67cc3f2b2bceb19659d23eb308c6dd04d
phongphat <phongphat.jap@gmail.com>
2025-02-11 15:09:41 +0700

    udpate Logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,18 +1,22 @@
 import useAuthStore from "../store/auth-store";
-
+import { createAlert } from "../utils/createAlert";
+import { useNavigate } from "react-router";
 function Logout() {
   // JS
   const actionLogout = useAuthStore((state) => state.actionLogout);
+  const navigate = useNavigate();
 
   const hdlLogout = () => {
     // code body
-    console.log("Hello, Logout");
+    // console.log("Hello, Logout");
+    createAlert("success", "Logout Sucess");
+    actionLogout();
+    navigate("/");
   };
 
   return (
     <div className="text-white">
-      <button className=" hover:cursor-pointer" 
-      onClick={hdlLogout}>
+      <button className=" hover:cursor-pointer" onClick={hdlLogout}>
         Logout
       </button>
     </div>
