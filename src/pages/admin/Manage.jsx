db03fbe81eb0135a594a17c81b50acd5a3fddc0e
phongphat <phongphat.jap@gmail.com>
2025-02-11 15:49:43 +0700

    update fetchUser

--- src/pages/admin/Manage.jsx
+++ src/pages/admin/Manage.jsx
@@ -1,23 +1,60 @@
+import { useEffect, useState } from "react";
+import { actionListUsers } from "../../api/user";
+import useAuthStore from "../../store/auth-store";
+import { Trash2 } from "lucide-react";
+
 // rfce
 function Manage() {
+  // JS
+  const [users, setUsers] = useState([]);
+  const token = useAuthStore((state) => state.token);
+
+  console.log(users);
+
+  useEffect(() => {
+    // code
+    hdlFetchUsers(token);
+  }, []);
+
+  const hdlFetchUsers = async (token) => {
+    try {
+      const res = await actionListUsers(token);
+      setUsers(res.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.firstname}</td>
+                <td>{item.lastname}</td>
+                <td>{item.email}</td>
+                <td>{item.role}</td>
+                <td>
+                  <Trash2 color="red"/>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
